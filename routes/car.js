6b64588557c7c2d3dd13beb602cf73dd7d290b9c
NelliTurpeinen <71337997+NelliTurpeinen@users.noreply.github.com>
2021-02-15 09:50:26 +0200

    Update car.js

--- routes/car.js
+++ routes/car.js
@@ -13,7 +13,7 @@ router.get('/:id?',
       }
     });
   } else {
-    car.getAllcars(function(err, dbResult) {
+    car.get(function(err, dbResult) {
       if (err) {
         response.json(err);
       } else {
